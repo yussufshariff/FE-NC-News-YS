e996502272baa007588043cc11bf50356a3959dc
Yussuf Shariff <yussufshariff@outlook.com>
2023-02-07 10:47:06 +0000

    Basic Bootstrap Nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import ArticlesList from "./Components/ArticlesList";
-import Nav from "./Components/NavBar";
 import HomePage from "./Components/HomePage";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Routes } from "react-router-dom";
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
+      <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/articles" element={<ArticlesList />} />
